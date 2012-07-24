5312a615fe98798a32d568b3b2b39f76417c2a0f
Matt Snyder <matt@cranialpulse.com>
2012-07-24 15:43:18 -0400

    First test in place, sketching out top level calls

---
+++ lib/problem_one/feature.rb
@@ -0,0 +1,7 @@
+module ProblemOne
+  class Feature
+    def to_json
+      '{"feature":"TXT MESSAGING - 250","date_range":"09/29 - 10/28","price":4.99}'
+    end
+  end
+end
---
+++ lib/problem_one/feature_parser.rb
@@ -0,0 +1,9 @@
+require './lib/problem_one/feature'
+
+module ProblemOne
+  class FeatureParser
+    def self.parse(feature_string)
+      Feature.new
+    end
+  end
+end
